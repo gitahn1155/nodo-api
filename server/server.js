a53ab18b915bf359183f0b7075d24a844a920a2b
Sungjun Ahn <gitahn1155@gmail.com>
2018-12-01 16:46:06 +0900

    Edit multiple files

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var bodyParser = require('body-parser'); //convert json to object
 var {ObjectID} = require('mongodb');
 
-//Here express route handler is here*
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -13,57 +12,45 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-    var todo = new Todo({
-      text: req.body.text
-    });
+  var todo = new Todo({
+    text: req.body.text
+  });
 
-    todo.save().then((doc) => {
-      res.send(doc);
-    }, (e) => {
-      res.status(400).send(e);
-    });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
-
 
 app.get('/todos', (req, res) => {
-   Todo.find().then((todos) => {
-       res.send({todos});
-   }, (e) => {
-       res.status(400).send(e);
-   }) 
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-// GET /todos/123453
 app.get('/todos/:id', (req, res) => {
-   var id = req.params.id;
-    
-    if (!ObjectID.isValid(id)) {
-        return res.status(404).send();
-    }
-    
-    Todo.findById(id).then((todo) => {
-        if (!todo) {
-            return res.status(404).send();
-        }
-        
-        res.send({todo});    
-    }).catch((e) => {
-        res.status(400).send();
-    })
-    
-   //Valid id using isValid
-   // 404 - send back empty send
+  var id = req.params.id;
 
-    //findById 
-    // success
-        //If todo - send it back
-        //If no todo - send back 404 with empty body
-    // error
-        // 400 - and send empty body back
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
-   console.log(`Started on port ${port}`);
+  console.log(`Started up at port ${port}`);
 });
 
 //POST HTTP - send resource as body, json which is text property
